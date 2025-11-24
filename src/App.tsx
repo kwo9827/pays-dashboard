b5d435e9608ef8f906d88d28af59518a80127294
오승열 <tmdduf785@naver.com>
2025-11-24 16:54:21 +0900

    chore: initial settings

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-8">
+      <div className="rounded-2xl border-2 border-gray-400 p-6 shadow-xl">
+        Tailwind TEST 5 — Card Style
+      </div>
+    </div>
+  );
+}
